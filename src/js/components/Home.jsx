185723eedd63c980e76179e1fb6960e21f4435ed
SaraMarti <saramartialcocel@gmail.com>
2026-07-10 20:14:43 +0200

    semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [shineColor, setShineColor] = useState("roja")
+	const [violetaVisible, setVioletaVisible] = useState(false)
+	console.log(setShineColor)
+
+	return (
+		<div className="principal">
+
+			<div className="brazo-horizontal">
+				<div className="tallo">
+					<div className="semaforo"
+
+					>
+						<div className={shineColor === "roja" ? "luz roja activa" : "luz roja"} onClick={() => setShineColor("roja")}></div>
+						<div className={shineColor === "amarilla" ? "luz amarilla activa" : "luz amarilla"} onClick={() => setShineColor("amarilla")}></div>
+						<div className={shineColor === "verde" ? "luz verde activa" : "luz verde"} onClick={() => setShineColor("verde")}></div>
+						{
+							violetaVisible && (
+								<div className={shineColor === "violeta" ? "luz violeta activa" : "luz violeta"} ></div>
+							)
+						}
+					</div>
+				</div>
+			</div>
+
+			<div className="poste-vertical">
+				<div className="boton-peaton">
+					<div className="btn "
+					 onClick={() => {  setVioletaVisible(!violetaVisible)
+						setShineColor("violeta")
+					} }></div>
+					<div className="btn uno" 
+					onClick={() => setShineColor(shineColor === "roja" ? "amarilla" : shineColor === "amarilla" ? "verde" : "roja")} ></div>
+				</div>
+			
+			</div>
+
 		</div>
 	);
 };
